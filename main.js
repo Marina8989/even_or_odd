1f3fe64d58c87b15851bc21f98181210be1c4b96
Marina8989 <marina.mill209@gmail.com>
2019-03-28 10:22:02 -0700

    check if even or odd

---
+++ main.js
@@ -0,0 +1,10 @@
+function even_or_odd(num) {
+  if (num % 2 === 0) {
+    return "Even";
+  }
+  return "Odd";
+}
+
+console.log(even_or_odd(4));
+console.log(even_or_odd(17));
+console.log(even_or_odd(74485645));
